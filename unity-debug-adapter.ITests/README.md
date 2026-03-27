27100f7bfc7f6533897fc87cce68c080115ddaba
Walid Chtioui <walid.chtioui@ensi-uma.tn>
2026-03-27 17:19:23 +0100

    tmp

--- unity-debug-adapter.ITests/README.md
+++ unity-debug-adapter.ITests/README.md
@@ -1,10 +1,17 @@
 # unity-debug-adapter Tests
 
-Currently we are only running a single integration tests that involves running
-an nvim-dap frontend (via Neovim server instance) on a simply C# scipt that is
-part of a Unity project.
+Currently we are only running a single end-to-end test that involves:
+
+1. instantiating a Unity Editor instance with the project `./unity_test_project`
+opened.
+1. parsing requests from `./log.txt` and forwarding them to unity-dap then
+asserting the responses with the responses in the `./log.txt`.
+1. initially, I wanted this test to be trully end-to-end by running a Neovim
+front-end DAP client (nvim-dap), running this program (unity-dap), and running
+a Unity Editor project instance. That prooved to be very difficult to achieve
+(especially running Neovim dap front-end) hence why I chose this approach
+instead.
 
 Requirements:
 
-- Neovim >= 0.11 with nvim-dap plugin client installed
 - Unity >= 2019.4
--- unity-debug-adapter.ITests/UnityDebugSession_FullSessionMockTest.cs
+++ unity-debug-adapter.ITests/UnityDebugSession_FullSessionMockTest.cs
@@ -1,15 +1,35 @@
 ﻿using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using NUnit.Framework;
+using UnityDebugAdapter;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System;
+using System.Text;
 
 namespace unity_debug_adapter.ITests
 {
+  /// <summary>
+  /// End-to-End testing of the Unity debug session. Initially, I thought about making this trully end-to-end but
+  /// launching Neovim, setting up breakpoints, steping in/out is simply too much work and too error-prone.
+  ///
+  /// What I do instead is to supply DAP requests (that were captured in a real debugging session from Neovim <-> Unity)
+  /// and send them to this debug adapter and assert the responses (here I am assuming they remain the same - in case
+  /// they don't the response has to be parsed and only fields we care about have to be asserted).
+  ///
+  ///   log.txt: contains a request per line (without the Content-Length: (\d+)\r\n\r\n sequence)
+  /// </summary>
   [TestFixture]
   public class UnityDebugSession_FullSessionMockTest
   {
     // private static readonly Regex UNITY_VERSION_REGEX = new Regex(@"(\d+)\.(\d+)");
     private Process m_UnityProcess;
+    private Process m_DebugAdapterProcess;
+    private readonly Regex re = new Regex(@"Content-Length: (\d+)\r\n\r\n");
 
     [OneTimeSetUp]
     public void StartTest()
@@ -50,23 +70,172 @@ namespace unity_debug_adapter.ITests
         Assert.Fail($"could not find Unity Editor 2022.3.X installed (looked in {unity_hub_editor_dir})");
       }
 
+      // if the provided Unity project is invalid, Unity simply doesn't launch and weirdly exits with a 0 exit code
+      string unity_test_project = @"C:\Users\walid\Desktop\unity_test_project";  // Path.GetFullPath("./unity_test_project")
       Trace.TraceInformation($"Unity executable is set to {unity_exe}");
 
       // start debuggee (i.e., Unity) on the unity_test_project
       m_UnityProcess = new Process();
-      m_UnityProcess.StartInfo.FileName = unity_exe;  // -batchmode -nographics -wait-for-managed-debugger 
-      m_UnityProcess.StartInfo.Arguments = $"-projectPath {Path.GetFullPath("./unity_test_project")}";
+      m_UnityProcess.StartInfo.FileName = unity_exe;  // -batchmode -nographics
+      m_UnityProcess.StartInfo.Arguments = $"-projectPath {unity_test_project}";
       m_UnityProcess.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
       m_UnityProcess.StartInfo.CreateNoWindow = false;
       m_UnityProcess.StartInfo.UseShellExecute = false;
-      m_UnityProcess.StartInfo.RedirectStandardOutput = true;
-      m_UnityProcess.StartInfo.RedirectStandardError = true;
-      m_UnityProcess.StartInfo.RedirectStandardInput = true;
+      m_UnityProcess.StartInfo.RedirectStandardOutput = false;
+      m_UnityProcess.StartInfo.RedirectStandardError = false;
+      m_UnityProcess.StartInfo.RedirectStandardInput = false;
       m_UnityProcess.Start();
 
-      Trace.TraceInformation($"started process: {m_UnityProcess.StartInfo.FileName} {m_UnityProcess.StartInfo.Arguments}");
+      Trace.TraceInformation($"started Unity Editor process: {m_UnityProcess.StartInfo.FileName} {m_UnityProcess.StartInfo.Arguments}");
+
+      // start debug adapter in another child process
+      m_DebugAdapterProcess = new Process();
+      m_DebugAdapterProcess.StartInfo.FileName = "../bin/Release/unity-debug-adapter.exe";
+      m_DebugAdapterProcess.StartInfo.Arguments = "--log-level=none";
+      m_DebugAdapterProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+      m_DebugAdapterProcess.StartInfo.CreateNoWindow = true;
+      m_DebugAdapterProcess.StartInfo.UseShellExecute = false;
+      m_DebugAdapterProcess.StartInfo.RedirectStandardOutput = true;
+      m_DebugAdapterProcess.StartInfo.RedirectStandardError = true;
+      m_DebugAdapterProcess.StartInfo.RedirectStandardInput = true;
+      m_DebugAdapterProcess.Start();
+
+      Trace.TraceInformation($"started debug adapter process: {m_DebugAdapterProcess.StartInfo.FileName} {m_DebugAdapterProcess.StartInfo.Arguments}");
+
+      // first, filter out responses/requests from the log file and save them in a string list
+      var requests = new SortedDictionary<int, string>();
+      var responses = new Dictionary<int, string>();
+      int maxResponseLen = 0;
+
+      foreach (string l in File.ReadAllLines("./log.txt"))
+      {
+        // because the logger logs \r\n\r\n sequence as rnrn
+        var _l = l.Replace("rnrn", "\r\n\r\n");
+        var m = re.Match(_l);
+        if (!m.Success || m.Groups.Count < 2)
+          continue;
+
+        // l is always encoded in UTF8 so we can safely just use Length
+
+        string body = _l.Substring(m.Index + "Content-Length: ".Length + m.Groups[1].Length + 4);
+        var parsedJson = JObject.Parse(body);
+        if (parsedJson == null)
+        {
+          // TODO: error out
+          continue;
+        }
+
+        var _type = (string?)parsedJson["type"];
+        if (string.IsNullOrWhiteSpace(_type))
+        {
+          // TODO: fail test if string is null
+          continue;
+        }
+
+        // don't care about events for the moment
+        if (_type == "event")
+          continue;
+
+        if (_type == "request")
+        {
+          var request_seq = (int?)parsedJson["seq"];
+          if (request_seq == null)
+          {
+            // TODO: error out
+            continue;
+          }
+          requests.Add(request_seq.Value, _l.Substring(m.Index));
+          continue;
+        }
+
+        if (_type == "response")
+        {
+          var request_seq = (int?)parsedJson["request_seq"];
+          if (request_seq == null)
+          {
+            // TODO: error out
+            continue;
+          }
+          responses.Add(request_seq.Value, _l.Substring(m.Index));
+          maxResponseLen = Math.Max(maxResponseLen, _l.Length - m.Index);
+          continue;
+        }
+
+        // TODO: error
+      }
+
+      Trace.TraceInformation($"parsed {responses.Count} responses from log.txt");
+      Trace.TraceInformation($"parsed {requests.Count} requests from log.txt");
+      Trace.TraceInformation($"max response length: {maxResponseLen}");
+
+      char[] buffer = new char[maxResponseLen];
+      foreach (var request in requests)
+      {
+        int requestSeq = request.Key;
+        string requestStr = request.Value;
+        m_DebugAdapterProcess.StandardInput.Write(requestStr);
+        // now wait for response
+        var nbrCharsReceived = m_DebugAdapterProcess.StandardOutput.Read(buffer, 0, buffer.Length);
+        var responseStr = new string(buffer, 0, nbrCharsReceived);
+        if (string.IsNullOrWhiteSpace(responseStr))
+        {
+          // TODO: test fail here
+          continue;
+        }
+
+        var parsedJson = JObject.Parse(responseStr);
+        if (parsedJson == null)
+        {
+          // TODO: test fail here
+          continue;
+        }
+
+        var _requestSeq = (int?)parsedJson["request_seq"];
+        if (_requestSeq == null)
+        {
+          // TODO: test fail here
+          continue;
+        }
+
+        // fetch the response from the stored responses from log.txt
+        string? expectedResponse = responses[_requestSeq.Value];
+        if (string.IsNullOrWhiteSpace(expectedResponse))
+        {
+          // TODO: test fail here
+          continue;
+        }
+
+        // TODO: test if expectedResponse == response
+
+      }
+
+      /*
+      foreach (string request in requests)
+      {
+
+        m_DebugAdapterProcess.StandardInput.Write(request);
+        // now wait for a response
+        string response = m_DebugAdapterProcess.StandardOutput.ReadLine();
+
+        var m = re.Match(response);
+
+        // TODO: get 
+
+        m_DebugAdapterProcess.StandardInput.Write($"Content-Length: ({l.Length})\r\n\r\n{l}");
+        //  then wait for the response on stdout
+        string response = m_DebugAdapterProcess.StandardOutput.ReadLine();
+        // Assert
+        Assert.That(response);
+      }
+      */
+
+      RunSession(Console.OpenStandardInput(), Console.OpenStandardOutput());
 
       m_UnityProcess.WaitForExit();
+      if (m_UnityProcess.ExitCode != 0)
+      {
+        Assert.Fail($"Unity process exited with non-0 exit code: {m_UnityProcess.ExitCode}");
+      }
 
       Assert.Pass();
 
@@ -90,6 +259,19 @@ namespace unity_debug_adapter.ITests
         m_UnityProcess.Kill();
         m_UnityProcess.WaitForExit();
         m_UnityProcess.Dispose();
+      }
+      catch (System.InvalidOperationException)
+      {
+        // probably means that process has already exited
+      }
+
+      Trace.TraceInformation("killing debug adapter process ...");
+
+      try
+      {
+        m_DebugAdapterProcess.Kill();
+        m_DebugAdapterProcess.WaitForExit();
+        m_DebugAdapterProcess.Dispose();
       }
       catch (System.InvalidOperationException)
       {
